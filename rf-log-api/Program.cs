ef5e005c0fc743500c7e59ba0a3498b70846551e
Raymart Flores <raymart.flores@3cloudsolutions.com>
2024-05-22 07:51:28 +0800

    Logging config

--- rf-log-api/Program.cs
+++ rf-log-api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Logging.AzureAppServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,9 +11,16 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddLogging(loggingBuilder =>
 {
+    loggingBuilder.ClearProviders();
     loggingBuilder.AddConsole();
     loggingBuilder.AddDebug();
     loggingBuilder.AddAzureWebAppDiagnostics();
+});
+builder.Services.Configure<AzureFileLoggerOptions>(options =>
+{
+    options.FileName = "first-azure-log";
+    options.FileSizeLimit = 50 * 1024;
+    options.RetainedFileCountLimit = 10;
 });
 
 var app = builder.Build();
